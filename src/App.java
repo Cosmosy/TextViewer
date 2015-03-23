fa26239f42ee866ca7983786ea7a91ac2ef3e3c5
Cosmosy <william.robinson@my.jcu.edu.au>
2015-03-23 15:41:04 +1000

    Initial Index

---
+++ src/App.java
@@ -0,0 +1,14 @@
+/**
+ * Text Viewer App - prints the contents of a string to console
+ * Created by William Robinson on 23/03/2015.
+ */
+public class App {
+    public static void main(String[] args){
+        String text = "This is a simple test\nOne Two Three \nHello World!";
+
+        for (int i = 0; i < text.length(); ++i){
+            System.out.print(text.charAt(i));
+        }
+        System.out.println();
+    }
+}
